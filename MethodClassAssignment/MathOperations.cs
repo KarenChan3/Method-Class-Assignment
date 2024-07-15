6250e1b325fb2d29bafb51cf6667f8c6498839ba
PatrickRiel <chukne@gmail.com>
2024-07-15 07:00:20 -0400

    Initial commit with MathOperations class and Main method

---
+++ MethodClassAssignment/MathOperations.cs
@@ -0,0 +1,15 @@
+﻿using System; 
+
+public class MathOperations
+{
+    public void PerformOperation(int number1, int number2)
+    {
+        // perform a math operation.
+        int result = number1 + 10;
+
+        // display results.
+        Console.WriteLine($"The result of the operation is: {result}");
+        Console.WriteLine($"The second number is: {number2}");
+    }
+}
+
---
+++ MethodClassAssignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // create an instance of MathOperations.
+        MathOperations mathOps = new MathOperations();
+
+        // call PerformOperation method with parameters.
+        mathOps.PerformOperation(5, 20);
+
+        // call PerformOperation method with named parameters.
+        mathOps.PerformOperation(number1: 7, number2: 30);
+
+        // keep console window open to review output.
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+}
+
